46095f28d7cf946dc64c441293652fae3397aa3a
Imanol Cea <imcea@mercadona.es>
2019-12-31 15:22:14 +0100

    Rename variables in test

--- test/postoffice_web/controllers/api/topic_controller_test.exs
+++ test/postoffice_web/controllers/api/topic_controller_test.exs
@@ -13,7 +13,7 @@ defmodule PostofficeWeb.Api.TopicControllerTest do
   end
 
   describe "create topic" do
-    test "renders topic when data is valid", %{conn: conn} do
+    test "renders created topic information when data is valid", %{conn: conn} do
       conn = post(conn, Routes.api_topic_path(conn, :create), topic: @create_attrs)
       created_topic = json_response(conn, 201)["data"]
       assert created_topic["name"] == "test"
@@ -25,10 +25,10 @@ defmodule PostofficeWeb.Api.TopicControllerTest do
     end
 
     test "do not create topic in case it already exists", %{conn: conn} do
-      {:ok, topic} = Messaging.create_topic(%{name: "test"})
+      {:ok, existing_topic} = Messaging.create_topic(%{name: "test"})
       conn = post(conn, Routes.api_topic_path(conn, :create), topic: @create_attrs)
-      created_topic = json_response(conn, 201)["data"]
-      assert created_topic["id"] == topic.id
+      new_topic = json_response(conn, 201)["data"]
+      assert new_topic["id"] == existing_topic.id
     end
   end
 end
