93a2410ef5e0d8b890acbcf96f524884527eb067
Imanol Cea <imcea@mercadona.es>
2020-01-27 15:15:52 +0100

    Log before creating message success

--- lib/postoffice/handlers/http.ex
+++ lib/postoffice/handlers/http.ex
@@ -8,13 +8,14 @@ defmodule Postoffice.Handlers.Http do
   def run(publisher_endpoint, publisher_id, message) do
     case impl().publish(publisher_endpoint, message) do
       {:ok, message = %Message{}} ->
+        Logger.info("Succesfully sent http message to #{publisher_endpoint}")
+
         {:ok, _} =
           Messaging.create_publisher_success(%{
             publisher_id: publisher_id,
             message_id: message.id
           })
 
-        Logger.info("Succesfully sent http message to #{publisher_endpoint}")
         {:ok, :sent}
 
       {:error, status} ->
