9b1e01ae4b0131db96435155e5fbecf3326050a5
Imanol Cea <imcea@mercadona.es>
2020-01-31 16:45:14 +0100

    Do not create socket as its not used

--- lib/postoffice_web/endpoint.ex
+++ lib/postoffice_web/endpoint.ex
@@ -1,10 +1,6 @@
 defmodule PostofficeWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :postoffice
 
-  socket "/socket", PostofficeWeb.UserSocket,
-    websocket: true,
-    longpoll: false
-
   # Serve at "/" the static files from "priv/static" directory.
   #
   # You should set gzip to true if you are running phx.digest
