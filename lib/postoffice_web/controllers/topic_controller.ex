2164d3558b6a8f89d3b8126c3ad9bc8c37432858
Santiago Lanus Herrera <1985181+sanntt@users.noreply.github.com>
2020-02-03 16:17:06 +0100

    Add topic views

---
+++ lib/postoffice_web/controllers/topic_controller.ex
@@ -0,0 +1,27 @@
+defmodule PostofficeWeb.TopicController do
+  use PostofficeWeb, :controller
+
+  alias Postoffice.Messaging
+  alias Postoffice.Messaging.Topic
+
+  def index(conn, _params) do
+    topics = Messaging.list_topics()
+    render(conn, "index.html", topics: topics)
+  end
+
+  def new(conn, _params) do
+    changeset = Topic.changeset(%Topic{}, %{})
+
+    render(conn, "new.html", changeset: changeset)
+  end
+
+  def create(conn, %{"topic" => topic_params}) do
+    {:ok, topic} = Postoffice.create_topic(topic_params)
+
+    conn
+    |> put_flash(:info, "Topic #{topic.id} created!")
+    |> redirect(to: Routes.topic_path(conn, :index))
+  end
+
+
+end
