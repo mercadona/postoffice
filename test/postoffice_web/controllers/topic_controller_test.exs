f33a369331612e876430a813c7ae32874b30107c
Santiago Lanus <1985181+sanntt@users.noreply.github.com>
2020-02-04 17:41:27 +0100

    Add topic views (#38)
    * Add topic views
    
    * Add tests for get topics
    
    * Add topic creationg test
    
    Co-authored-by: Imanol Cea <lonamiaec@users.noreply.github.com>

---
+++ test/postoffice_web/controllers/topic_controller_test.exs
@@ -0,0 +1,35 @@
+defmodule PostofficeWeb.TopicControllerTest do
+  use PostofficeWeb.ConnCase
+
+  alias Postoffice.Fixtures
+
+  setup do
+    {:ok, conn: Phoenix.ConnTest.build_conn()}
+  end
+
+  describe "list topics" do
+    test "can access to topics list", %{conn: conn} do
+
+      conn
+      |> get(Routes.topic_path(conn, :index))
+      |> html_response(200)
+    end
+
+    test "all created topics are listed", %{conn: conn} do
+      topic = Fixtures.create_topic()
+
+      conn
+      |> get(Routes.topic_path(conn, :index))
+      |> html_response(200) =~ topic.name
+    end
+  end
+  
+  describe "create topics" do
+    test "can access to topics list", %{conn: conn} do
+
+      conn
+      |> post(Routes.topic_path(conn, :create, [topic: %{name: "Test Topic"}]))
+      |> redirected_to() == "/topics"
+    end
+  end
+end
