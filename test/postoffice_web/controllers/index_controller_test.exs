9012193e0780f2403bd3db90b8f6258656780fee
Imanol Cea <lonamiaec@users.noreply.github.com>
2020-02-10 14:52:06 +0100

    Add tests for postoffice_web (#44)
    * Add tests for postoffice_web
    
    * Fix type

---
+++ test/postoffice_web/controllers/index_controller_test.exs
@@ -0,0 +1,17 @@
+defmodule PostofficeWeb.IndexControllerTest do
+  use PostofficeWeb.ConnCase
+
+  setup do
+    {:ok, conn: Phoenix.ConnTest.build_conn()}
+  end
+
+  describe "index dashboard" do
+    test "can access", %{conn: conn} do
+      conn =
+        conn
+        |> get(Routes.dashboard_path(conn, :index))
+
+      assert html_response(conn, 200)
+    end
+  end
+end
