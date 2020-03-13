33b9728af03a239c641f68cea6087b856d23907a
Imanol Cea <lonamiaec@users.noreply.github.com>
2020-03-13 10:03:54 +0100

    Prepare v0.5.0 release (#59)

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Postoffice.MixProject do
   def project do
     [
       app: :postoffice,
-      version: "0.4.1",
+      version: "0.5.0",
       elixir: "~> 1.9",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
