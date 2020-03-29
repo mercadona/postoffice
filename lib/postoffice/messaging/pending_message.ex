66f3689bb97d5fdbf491d59ee0ffd8a89ea4e54d
juanjo <jponzvan@mercadona.es>
2020-03-29 21:21:26 +0200

    Add pending message

---
+++ lib/postoffice/messaging/pending_message.ex
@@ -0,0 +1,19 @@
+defmodule Postoffice.Messaging.PendingMessage do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "pending_message" do
+    belongs_to :topic, Postoffice.Messaging.Topic
+    belongs_to :message, Postoffice.Messaging.Message
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(pending_message, attrs) do
+    pending_message
+    |> cast(attrs, [:topic_id, :message_id])
+    |> validate_required([:topic_id, :message_id])
+  end
+
+end
---
+++ priv/repo/migrations/20200329191611_add_pending_message_schema.exs
@@ -0,0 +1,11 @@
+defmodule Postoffice.Repo.Migrations.AddPendingMessageSchema do
+  use Ecto.Migration
+
+  def change do
+    create table(:pending_messages) do
+      add :topic_id, references(:topics), null: false
+      add :message_id, references(:messages), null: false
+    end
+
+  end
+end
