64995406ed9d9656e51f3336c8992cf9cf22ff97
juanjo <jponzvan@mercadona.es>
2020-05-09 21:02:48 +0200

    Add algorythm

--- lib/postoffice/pubsub_ingester/pubsub_ingester.ex
+++ lib/postoffice/pubsub_ingester/pubsub_ingester.ex
@@ -1,5 +1,7 @@
 defmodule Postoffice.PubSubIngester.PubSubIngester do
   def run(topic) do
-
+# Obtenemos los mensajes de PubSub a través del cliente de google
+# Marcamos los mensajes como pendientes
+# Bebemos una cerveza y somos felices :)
   end
 end
--- test/postoffice/pubsub_ingester/pubsub_ingester_test.exs
+++ test/postoffice/pubsub_ingester/pubsub_ingester_test.exs
@@ -1,23 +1,30 @@
 defmodule Postoffice.PubSubIngester.PubSubIngesterTest do
+  use Postoffice.DataCase, async: true
   use ExUnit.Case, async: true
 
   import Mox
 
   alias Postoffice.Fixtures
   alias Postoffice.Messaging
+  alias Postoffice.Messaging.PendingMessage
   alias Postoffice.PubSubIngester.PubSubIngester
 
   setup [:set_mox_from_context, :verify_on_exit!]
 
-  setup do
-    :ok = Ecto.Adapters.SQL.Sandbox.checkout(Postoffice.Repo)
-  end
+  # setup do
+  #   :ok = Ecto.Adapters.SQL.Sandbox.checkout(Postoffice.Repo)
+  # end
 
   describe "pubsub ingester" do
     test "no message created if no undelivered message found" do
       PubSubIngester.run('topic-name')
 
       assert Messaging.list_messages() == []
+    end
+    test "create message when is ingested" do
+      PubSubIngester.run('topic-name')
+
+      assert length(Repo.all(PendingMessage)) == 1
     end
   end
 end
