04c61bec436ab8f456ad5d14635bfcd85ccfb439
juanjo <jponzvan@mercadona.es>
2020-05-18 20:22:11 +0200

    Implement real adapter

--- lib/postoffice/pubsub_ingester/adapters/impl.ex
+++ lib/postoffice/pubsub_ingester/adapters/impl.ex
@@ -2,6 +2,6 @@ defmodule Postoffice.PubSubIngester.Adapters.Impl do
   @moduledoc false
 
   @callback get(conn :: Map, sub_name :: String) :: {:ok, Map} | {:error, reason :: String}
-  @callback confirm(conn :: Map, ack_ids :: List) :: {:ok, Map}
+  @callback confirm(conn :: Map, ack_ids :: List, sub_name :: String) :: {:ok, Map}
   @callback connect() :: Map
 end
--- lib/postoffice/pubsub_ingester/adapters/pubsub.ex
+++ lib/postoffice/pubsub_ingester/adapters/pubsub.ex
@@ -1,11 +1,36 @@
 defmodule Postoffice.PubSubIngester.Adapters.PubSub do
   @behaviour Postoffice.PubSubIngester.Adapters.Impl
 
-  def get(conn, topic_subscription_relation) do
-    {:fake}
+  def get(conn, sub_name) do
+    GoogleApi.PubSub.V1.Api.Projects.pubsub_projects_subscriptions_pull(
+      conn,
+      get_project_id(),
+      sub_name,
+      body: %GoogleApi.PubSub.V1.Model.PullRequest{
+        maxMessages: get_max_messages()
+      }
+    )
   end
 
-  def confirm(ackIds) do
-    {:fake}
+  def confirm(conn, ackIds, sub_name) do
+    GoogleApi.PubSub.V1.Api.Projects.pubsub_projects_subscriptions_acknowledge(
+      conn,
+      get_project_id(),
+      sub_name,
+      body: %GoogleApi.PubSub.V1.Model.AcknowledgeRequest{
+        ackIds: ackIds
+      }
+    )
   end
+
+  def connect() do
+    {:ok, token} = Goth.Token.for_scope("https://www.googleapis.com/auth/cloud-platform")
+    GoogleApi.PubSub.V1.Connection.new(token.token)
+  end
+
+  defp get_project_id,
+    do: Application.get_env(:postoffice, :gcloud_pubsub_project_id, 'test')
+
+  defp get_max_messages,
+    do: Application.get_env(:postoffice, :pubsub_max_messages, 10)
 end
