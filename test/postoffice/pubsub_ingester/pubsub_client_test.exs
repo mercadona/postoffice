052537763e2b39284ba584e9c349a87ba32cfd03
juanjo <jponzvan@mercadona.es>
2020-05-18 19:33:23 +0200

    Extract fixtures

--- test/postoffice/pubsub_ingester/pubsub_client_test.exs
+++ test/postoffice/pubsub_ingester/pubsub_client_test.exs
@@ -9,75 +9,6 @@ defmodule Postoffice.PubSubIngester.PubSubClientTest do
 
   @without_messages {:ok, %GoogleApi.PubSub.V1.Model.PullResponse{receivedMessages: nil}}
 
-  @pubsub_error {:error,
-                 %Tesla.Env{
-                   __client__: %Tesla.Client{
-                     adapter: nil,
-                     fun: nil,
-                     post: [],
-                     pre: [
-                       {Tesla.Middleware.Headers, :call,
-                        [
-                          [
-                            {"authorization",
-                             "Bearer ya29.c.Ko8BywcP1ge4PXhiA4y9_qgO7P1qDmSwntuVMz0eMZUJKUH5eIezGJ0ZTNqEFFz4jMHwoKH9gFKvwaA9f6ty3hD6wkciYkfvbS74ebxr8usYuMmTi3ZOzGkyv4meYnj5yu37wtbsPtyOCSZSgT4BL9QtbG5_f1T9fFtqklhGkTROfU1F7UZYkXny47fXO53up8c"}
-                          ]
-                        ]}
-                     ]
-                   },
-                   __module__: GoogleApi.PubSub.V1.Connection,
-                   body:
-                     "{\n  \"error\": {\n    \"code\": 404,\n    \"message\": \"Resource not found (resource=supply-test-juan).\",\n    \"status\": \"NOT_FOUND\"\n  }\n}\n",
-                   headers: [
-                     {"cache-control", "private"},
-                     {"date", "Sun, 17 May 2020 19:19:38 GMT"},
-                     {"accept-ranges", "none"},
-                     {"server", "ESF"},
-                     {"vary", "X-Origin"},
-                     {"content-length", "130"},
-                     {"content-type", "application/json; charset=UTF-8"},
-                     {"x-xss-protection", "0"},
-                     {"x-frame-options", "SAMEORIGIN"},
-                     {"x-content-type-options", "nosniff"},
-                     {"alt-svc",
-                      "h3-27=\":443\"; ma=2592000,h3-25=\":443\"; ma=2592000,h3-T050=\":443\"; ma=2592000,h3-Q050=\":443\"; ma=2592000,h3-Q049=\":443\"; ma=2592000,h3-Q048=\":443\"; ma=2592000,h3-Q046=\":443\"; ma=2592000,h3-Q043=\":443\"; ma=2592000,quic=\":443\"; ma=2592000; v=\"46,43\""}
-                   ],
-                   method: :post,
-                   opts: [],
-                   query: [],
-                   status: 404,
-                   url:
-                     "https://pubsub.googleapis.com/v1/projects/itg-mimercadona/subscriptions/supply-test-juan:pull"
-                 }}
-
-  @two_messages {
-    :ok,
-    %GoogleApi.PubSub.V1.Model.PullResponse{
-      receivedMessages: [
-        %GoogleApi.PubSub.V1.Model.ReceivedMessage{
-          ackId:
-            "ISE-MD5FU0RQBhYsXUZIUTcZCGhRDk9eIz81IChFEAcGTwIoXXkyVSFBXBoHUQ0Zcnxmd2tTGwMKEwUtVVsRDXptXFcnUAwccHxhcm1dEwIBQlJ4W3OK75niloGyYxclSoGxxaxvM7nUxvhMZho9XhJLLD5-MjVFQV5AEkw5AERJUytDCypYEU4E",
-          message: %GoogleApi.PubSub.V1.Model.PubsubMessage{
-            attributes: nil,
-            data: "eyJvbmUiOiJ0d28ifQ==",
-            messageId: "1152107770337164",
-            publishTime: ~U[2020-05-15 18:04:46.791Z]
-          }
-        },
-        %GoogleApi.PubSub.V1.Model.ReceivedMessage{
-          ackId:
-            "ISE-MD5FU0RQBhYsXUZIUTcZCGhRDk9eIz81IChFEAcGTwIoXXkyVSFBXBoHUQ0Zcnxmd2tTGwMKEwUtVVoRDXptXFcnUAwccHxhcm9eEwQFRFt-XnOK75niloGyYxclSoGxxaxvM7nUxvhMZho9XhJLLD5-MjVFQV5AEkw5AERJUytDCypYEU4E",
-          message: %GoogleApi.PubSub.V1.Model.PubsubMessage{
-            attributes: %{"att_one" => "one"},
-            data: "eyJ0d28iOiJ0aHJlZSJ9",
-            messageId: "1152107540571801",
-            publishTime: ~U[2020-05-15 18:05:30.225Z]
-          }
-        }
-      ]
-    }
-  }
-
   @argument %{
     topic: "test",
     sub: "fake_sub"
@@ -99,13 +30,15 @@ end
     end
 
     test "get error from pubsub returns the error", %{pubsub_conn: pubsub_conn} do
-      expect(PubSubMock, :get, fn conn, "fake_sub" -> @pubsub_error end)
+      expect(PubSubMock, :get, fn conn, "fake_sub" -> Fixtures.pubsub_error() end)
 
-      @pubsub_error = PubSubClient.get(pubsub_conn, @argument)
+      error = PubSubClient.get(pubsub_conn, @argument)
+
+      assert error == Fixtures.pubsub_error()
     end
 
     test "get messages when has messages to receive", %{pubsub_conn: pubsub_conn} do
-      expect(PubSubMock, :get, fn conn, "fake_sub" -> @two_messages end)
+      expect(PubSubMock, :get, fn conn, "fake_sub" -> Fixtures.two_google_pubsub_messages() end)
 
       {:ok, messages} = PubSubClient.get(pubsub_conn, @argument)
 
@@ -134,7 +67,7 @@ end
 
       ackIds = ["ackId1", "ackId2"]
 
-      ack_message = PubSubClient.confirm(ackIds, pubsub_conn)
+      ack_message = PubSubClient.confirm(pubsub_conn, ackIds)
 
       assert ack_message == ack_message
     end
--- test/support/fixtures.ex
+++ test/support/fixtures.ex
@@ -52,18 +52,91 @@ defmodule Postoffice.Fixtures do
       Messaging.create_publisher_failure(%{message_id: message.id, publisher_id: publisher.id})
   end
 
-  def pubsub_conn(), do: %Tesla.Client{
-    adapter: nil,
-    fun: nil,
-    post: [],
-    pre: [
-      {Tesla.Middleware.Headers, :call,
-       [
+  def pubsub_conn(),
+    do: %Tesla.Client{
+      adapter: nil,
+      fun: nil,
+      post: [],
+      pre: [
+        {Tesla.Middleware.Headers, :call,
          [
-           {"authorization",
-            "Bearer ya29.c.Ko8BywcJmQ044Tz44v_NoMQ03cXByM1rMjKSFKBWpjcCE2RLIDlxlWvlSXC8gSYtQTmdkRi-wA-mzFsSn37l1uV7TlbHq5rIqqdDbr746sECtpT5vF1JEskVLC2VsEBc-ukAT4C8hb-n1xXLw00S2M5kCBANtdSsbkeTG1I57fuIGN3dU3TSKtRzmZ0on5Anlgs"}
-         ]
-       ]}
-    ]
-  }
+           [
+             {"authorization",
+              "Bearer ya29.c.Ko8BywcJmQ044Tz44v_NoMQ03cXByM1rMjKSFKBWpjcCE2RLIDlxlWvlSXC8gSYtQTmdkRi-wA-mzFsSn37l1uV7TlbHq5rIqqdDbr746sECtpT5vF1JEskVLC2VsEBc-ukAT4C8hb-n1xXLw00S2M5kCBANtdSsbkeTG1I57fuIGN3dU3TSKtRzmZ0on5Anlgs"}
+           ]
+         ]}
+      ]
+    }
+
+  def two_google_pubsub_messages(),
+    do: {
+      :ok,
+      %GoogleApi.PubSub.V1.Model.PullResponse{
+        receivedMessages: [
+          %GoogleApi.PubSub.V1.Model.ReceivedMessage{
+            ackId:
+              "ISE-MD5FU0RQBhYsXUZIUTcZCGhRDk9eIz81IChFEAcGTwIoXXkyVSFBXBoHUQ0Zcnxmd2tTGwMKEwUtVVsRDXptXFcnUAwccHxhcm1dEwIBQlJ4W3OK75niloGyYxclSoGxxaxvM7nUxvhMZho9XhJLLD5-MjVFQV5AEkw5AERJUytDCypYEU4E",
+            message: %GoogleApi.PubSub.V1.Model.PubsubMessage{
+              attributes: nil,
+              data: "eyJvbmUiOiJ0d28ifQ==",
+              messageId: "1152107770337164",
+              publishTime: ~U[2020-05-15 18:04:46.791Z]
+            }
+          },
+          %GoogleApi.PubSub.V1.Model.ReceivedMessage{
+            ackId:
+              "ISE-MD5FU0RQBhYsXUZIUTcZCGhRDk9eIz81IChFEAcGTwIoXXkyVSFBXBoHUQ0Zcnxmd2tTGwMKEwUtVVoRDXptXFcnUAwccHxhcm9eEwQFRFt-XnOK75niloGyYxclSoGxxaxvM7nUxvhMZho9XhJLLD5-MjVFQV5AEkw5AERJUytDCypYEU4E",
+            message: %GoogleApi.PubSub.V1.Model.PubsubMessage{
+              attributes: %{"att_one" => "one"},
+              data: "eyJ0d28iOiJ0aHJlZSJ9",
+              messageId: "1152107540571801",
+              publishTime: ~U[2020-05-15 18:05:30.225Z]
+            }
+          }
+        ]
+      }
+    }
+
+  def pubsub_error(),
+    do:
+      {:error,
+       %Tesla.Env{
+         __client__: %Tesla.Client{
+           adapter: nil,
+           fun: nil,
+           post: [],
+           pre: [
+             {Tesla.Middleware.Headers, :call,
+              [
+                [
+                  {"authorization",
+                   "Bearer ya29.c.Ko8BywcP1ge4PXhiA4y9_qgO7P1qDmSwntuVMz0eMZUJKUH5eIezGJ0ZTNqEFFz4jMHwoKH9gFKvwaA9f6ty3hD6wkciYkfvbS74ebxr8usYuMmTi3ZOzGkyv4meYnj5yu37wtbsPtyOCSZSgT4BL9QtbG5_f1T9fFtqklhGkTROfU1F7UZYkXny47fXO53up8c"}
+                ]
+              ]}
+           ]
+         },
+         __module__: GoogleApi.PubSub.V1.Connection,
+         body:
+           "{\n  \"error\": {\n    \"code\": 404,\n    \"message\": \"Resource not found (resource=supply-test-juan).\",\n    \"status\": \"NOT_FOUND\"\n  }\n}\n",
+         headers: [
+           {"cache-control", "private"},
+           {"date", "Sun, 17 May 2020 19:19:38 GMT"},
+           {"accept-ranges", "none"},
+           {"server", "ESF"},
+           {"vary", "X-Origin"},
+           {"content-length", "130"},
+           {"content-type", "application/json; charset=UTF-8"},
+           {"x-xss-protection", "0"},
+           {"x-frame-options", "SAMEORIGIN"},
+           {"x-content-type-options", "nosniff"},
+           {"alt-svc",
+            "h3-27=\":443\"; ma=2592000,h3-25=\":443\"; ma=2592000,h3-T050=\":443\"; ma=2592000,h3-Q050=\":443\"; ma=2592000,h3-Q049=\":443\"; ma=2592000,h3-Q048=\":443\"; ma=2592000,h3-Q046=\":443\"; ma=2592000,h3-Q043=\":443\"; ma=2592000,quic=\":443\"; ma=2592000; v=\"46,43\""}
+         ],
+         method: :post,
+         opts: [],
+         query: [],
+         status: 404,
+         url:
+           "https://pubsub.googleapis.com/v1/projects/itg-mimercadona/subscriptions/supply-test-juan:pull"
+       }}
 end
