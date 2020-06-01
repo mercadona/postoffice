c075bdb929f8a77ccb2fda85e4f4b2687badd750
Imanol Cea <lonamiaec@users.noreply.github.com>
2020-06-01 16:48:55 +0200

    Bump version to 0.9 (#80)

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Postoffice.MixProject do
   def project do
     [
       app: :postoffice,
-      version: "0.8.0",
+      version: "0.9.0",
       elixir: "~> 1.9",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
