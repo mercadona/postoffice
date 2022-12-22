284967bbf015cfdeb71681127c8d3dfce72cad8a
Awais <mawais@mercadona.es>
2022-12-22 17:36:07 +0100

    added test for the delete action

--- test/postoffice_web/controllers/message_controller_test.exs
+++ test/postoffice_web/controllers/message_controller_test.exs
@@ -2,12 +2,14 @@ defmodule PostofficeWeb.MessageControllerTest do
   use PostofficeWeb.ConnCase, async: true
 
   alias Postoffice.Fixtures
+  alias Postoffice.Messaging
+  alias Postoffice.Repo
 
   setup do
     {:ok, conn: Phoenix.ConnTest.build_conn()}
   end
 
-  describe "list failed message" do
+  describe "failing messages" do
     test "can access to messages list", %{conn: conn} do
       failing_job = Fixtures.create_failing_message(%{id: 1, user_id: 2})
 
@@ -16,5 +18,17 @@ defmodule PostofficeWeb.MessageControllerTest do
 
       assert html_response(response, 200) =~ to_string(failing_job.id)
     end
+
+    test "cancel failing message job", %{conn: conn} do
+      failing_job = Fixtures.create_failing_message(%{id: 1, user_id: 2})
+
+      assert conn
+      |> delete(Routes.message_path(conn, :delete, failing_job.id))
+      |> redirected_to() == "/messages"
+
+      deleted_failing_job = Repo.get(Oban.Job, failing_job.id)
+      assert deleted_failing_job.state == "cancelled"
+    end
+
   end
 end
