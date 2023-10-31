ef1010abe8c13040a628b4cdfb43fe867a04fb7c
Awais <mawais@mercadona.es>
2023-10-31 16:43:29 +0100

    Added sentry (#181)
    * added sentry
    
    * removed 1/0

--- lib/postoffice_web/endpoint.ex
+++ lib/postoffice_web/endpoint.ex
@@ -1,4 +1,5 @@
 defmodule PostofficeWeb.Endpoint do
+  use Sentry.PlugCapture
   use Phoenix.Endpoint, otp_app: :postoffice
 
   @session_options [
@@ -38,6 +39,7 @@ defmodule PostofficeWeb.Endpoint do
     pass: ["*/*"],
     json_decoder: Phoenix.json_library()
 
+  plug Sentry.PlugContext
   plug Plug.MethodOverride
   plug Plug.Head
 
